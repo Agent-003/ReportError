5dfc1fdf163622253ca9d47e6fff4391e8bf30de
Anastasiia Abbasova <a.abbasova@dinarys.com>
2022-08-21 17:16:14 +0300

    Create empty module

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Pineapple. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Pineapple_ReportError', __DIR__);
